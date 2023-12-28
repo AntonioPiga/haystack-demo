c68dd60b529780c6f35a2bc2613a7921dc24c7ac
Antonio Piga <antonio.piga93@gmail.com>
2023-12-28 18:20:39 +0100

    feat: data and demo

--- demo.py
+++ demo.py
@@ -3,15 +3,15 @@ from haystack.utils import build_pipeline, add_example_data, print_answers
 
 # We are model agnostic :) Here, you can choose from: "anthropic", "cohere", "huggingface", and "openai".
 provider = "openai"
-API_KEY = "xxx"
+API_KEY = "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"
 
 document_store = InMemoryDocumentStore(use_bm25=True)
 
-add_example_data(document_store, "data/GoT_getting_started")
+add_example_data(document_store, "data/Antonio")
 
 pipeline = build_pipeline(provider, API_KEY, document_store)
 
 # Ask a question on the data you just added.
-result = pipeline.run(query="Who is the father of Arya Stark?")
+result = pipeline.run(query="What is the difference between Antonio and Luigi? If I have to choose one of them to work as a developer for my site, whom do you suggest?")
 
 print_answers(result, details="medium")
