b0e2d625998ebd79eb7be55b3bd4c66036a8e83b
Antonio Piga <antonio.piga93@gmail.com>
2024-01-02 16:27:18 +0100

    feat: env config.py and readme

--- demo.py
+++ demo.py
@@ -1,17 +1,17 @@
 from haystack.document_stores import InMemoryDocumentStore
 from haystack.utils import build_pipeline, add_example_data, print_answers
+from config import API_KEY
 
-# We are model agnostic :) Here, you can choose from: "anthropic", "cohere", "huggingface", and "openai".
+
+# can choose from: "anthropic", "cohere", "huggingface", and "openai".
 provider = "openai"
-API_KEY = "xxx"
 
 document_store = InMemoryDocumentStore(use_bm25=True)
 
-add_example_data(document_store, "data/Antonio")
+add_example_data(document_store, "data/People")
 
 pipeline = build_pipeline(provider, API_KEY, document_store)
 
-# Ask a question on the data you just added.
-result = pipeline.run(query="What is the difference between Antonio and Luigi? If I have to choose one of them to work as a developer for my site, whom do you suggest?")
+result = pipeline.run(query="What is the difference between Marco and Luigi?")
 
 print_answers(result, details="medium")
